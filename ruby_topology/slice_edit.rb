d58673a76bc188df7a51434bd8354877db590595
kuromike <t-nisimr@ist.osaka-u.ac.jp>
2014-01-21 11:18:05 +0900

    kadai submit

---
+++ ruby_topology/slice_edit.rb
@@ -0,0 +1,208 @@
+# coding: utf-8
+
+# スイッチ
+switches = Array.new
+n_switches = 0
+# ホスト
+hosts = Array.new
+hosts_ip = Array.new
+hosts_subnet = Array.new
+n_hosts = 0
+# リンク
+links = Array.new
+n_links = 0
+# スライス
+slice_names = Array.new # スライス名
+slice_hosts = Array.new # 配列を要素に持つ配列。ホストの添字を代入したい。
+slice_hostname = Array.new # ホスト名（これはtopology_controllerでしか使わない）
+slice_hostip = Array.new   # ホストのIP（上に同じ）
+slice_hostsnm = Array.new  # ホストのサブネットマスク（上に同じ）
+n_hosts_in_slice = Array.new
+n_slice = 0
+
+# ファイルパス
+fp_topology = open("sample1.conf","r")
+fp_slice2 = open("slice_temple.txt","r")
+fp_slice = open("slice.txt","r")
+fp_detail = open("detail.txt","w")
+
+# 現在読んでいる行
+n_readline = 0
+str_readlines = nil
+tempStrs = nil
+
+
+# 開始
+puts("引数1 : #{ARGV[0]}\n")
+puts("引数2 : #{ARGV[1]}\n")
+puts("引数3 : #{ARGV[3]}\n")
+
+fp_detail.write("[Start]\n")
+
+# トポロジを読み込み
+fp_topology.each do |line|
+  if line[0]=="#"
+    next  # コメント行は読み飛ばす
+  elsif line.strip.length==0
+    next  # 空白行は読み飛ばす
+  elsif line.include?("vswitch")
+    tempStrs = line.split("\"")
+    fp_detail.write("スイッチ\"#{tempStrs[1]}\"を検出\n")
+    switches[n_switches] = tempStrs[1]
+    n_switches += 1
+  elsif line.include?("vhost(")
+    tempStrs = line.split("\"")
+    tempStrs.each_index do |i| # パラメータを検出したい
+      if tempStrs[i].include?("ip ")
+        hosts_ip[n_hosts] = tempStrs[i+1]
+      elsif tempStrs[i].include?("netmask ")
+        hosts_subnet[n_hosts] = tempStrs[i+1]
+      end
+    end
+    fp_detail.write("ホスト\"#{tempStrs[1]}\"を検出。IPアドレス=#{hosts_ip[n_hosts]},サブネットマスク=#{hosts_subnet[n_hosts]}\n")
+    hosts[n_hosts] = tempStrs[1]
+    n_hosts += 1
+  elsif line.include?("link ")
+    tempStrs = line.split("\"")
+    fp_detail.write("リンク#{tempStrs[1]}->#{tempStrs[3]}を検出\n")
+  else
+    puts("? : #{line}")
+  end
+end
+
+
+# スライス情報を読み込み
+str_readlines = fp_slice.readlines
+# 1行目はスライス数
+str_readline = str_readlines[n_readline]
+n_slice = str_readline.to_i
+n_readline += 1
+for i in 0...n_slice do
+  slice_hosts[i] = Array.new
+  slice_hostname[i] = Array.new
+  slice_hostip[i] = Array.new
+  slice_hostsnm[i] = Array.new
+  # スライス名
+  str_readline = str_readlines[n_readline]
+  n_readline += 1
+  slice_names[i] = str_readline.chomp
+  # スライス内のホスト数
+  str_readline = str_readlines[n_readline]
+  n_readline += 1
+  n_hosts_in_slice[i] = str_readline.to_i
+  # 各ホスト情報
+  for j in 0...n_hosts_in_slice[i] do
+    str_readline = str_readlines[n_readline]
+    n_readline += 1
+    tempStrs = str_readline.chomp.split(",")
+    slice_hosts[i][j] = hosts.index( tempStrs[0] )
+    # 以下、topology_controllerのための格納文
+    slice_hostname[i][j] = tempStrs[0]
+    slice_hostip[i][j] = tempStrs[1]
+    slice_hostsnm[i][j] = tempStrs[2]
+  end
+end
+
+
+# スライス情報を更新
+if ARGV[0]=="create"
+  # スライス作成。スライス名を指定
+  puts(" -*- create -*- ")
+  if ARGV[1].nil?
+    puts("please input slice's name.")
+  elsif slice_names.include?(ARGV[1])
+    puts("slice \"#{ARGV[1]}\" arleady exists.")
+  else
+    slice_names[n_slice] = ARGV[1]
+    n_hosts_in_slice[n_slice] = 0
+    slice_hosts[n_slice] = Array.new
+    slice_hostname[n_slice] = Array.new
+    slice_hostip[n_slice] = Array.new
+    slice_hostsnm[n_slice] = Array.new
+    n_slice += 1
+    puts("slice \"#{ARGV[1]}\" was created.")
+  end
+elsif ARGV[0]=="addhost"
+  # ホスト追加。スライス名、ホスト名を指定
+  puts(" -*- addhost -*- ")
+  if ARGV[1].nil? || ARGV[2].nil?
+    puts("please input slice and host's name.")
+  else
+    n_targetslice = slice_names.index(ARGV[1])
+    if n_targetslice.nil?
+      puts("slice \"#{ARGV[1]}\" does not exist.")
+    else
+      n_targethost = hosts.index( ARGV[2] )
+      if slice_hosts[n_targetslice].include?(n_targethost)
+        puts("host \"#{ARGV[2]}\" arleady exists in slice \"#{ARGV[1]}\".")
+      elsif n_targethost.nil?
+        puts("host \"#{ARGV[2]}\" does not exist in topology(.conf).")
+      else
+        slice_hosts[n_targetslice][n_hosts_in_slice[n_targetslice]] = n_targethost
+        n_hosts_in_slice[n_targetslice] += 1
+        puts("host \"#{ARGV[2]}\" was added to slice \"#{ARGV[1]}\".")
+      end
+    end
+  end
+elsif ARGV[0]=="delhost"
+  # ホスト削除。スライス名、ホスト名を指定
+  puts(" -*- delhost -*- ")
+  if ARGV[1].nil? || ARGV[2].nil?
+    puts("please input slice and host's name.")
+  else
+    n_targetslice = slice_names.index(ARGV[1])
+    if n_targetslice.nil?
+      puts("slice \"#{ARGV[1]}\" does not exist.")
+    else
+      n_targethost = slice_hostname[n_targetslice].index(ARGV[2])
+      if n_targethost.nil?
+        puts("host \"#{ARGV[2]}\" does not exist in slice \"#{ARGV[1]}\".")
+      else
+        for x in n_targethost...n_hosts_in_slice[n_targetslice]-1 do
+          slice_hosts[n_targetslice][x] = slice_hosts[n_targetslice][x+1]
+        end
+        slice_hosts[n_targetslice][n_hosts_in_slice[n_targetslice]-1] = nil
+        n_hosts_in_slice[n_targetslice] -= 1
+        puts("host \"#{ARGV[2]}\" was deleted from slice \"#{ARGV[1]}\".")
+      end
+    end
+  end
+elsif ARGV[0]=="searchhost"
+  # ホスト検索。ホスト名のみ指定
+  puts(" -*- searchhost -*- ")
+else
+  puts(" -*- undefined command -*- ")
+end
+
+
+
+# スライス情報を書き込み
+fp_slice.close
+fp_slice = open("slice.txt","w")
+fp_slice.write("#{n_slice}\n")
+for i in 0...n_slice do
+  fp_slice.write("#{slice_names[i]}\n")
+  fp_slice.write("#{n_hosts_in_slice[i]}\n")
+  for j in 0...n_hosts_in_slice[i] do
+    p = slice_hosts[i][j]
+    fp_slice.write("#{hosts[p]},#{hosts_ip[p]},#{hosts_subnet[p]}\n")
+  end
+end
+
+
+# 終了
+# puts("スイッチ : \n#{switches.to_s}\n")
+# puts("ホスト : \n#{hosts.to_s}\n")
+# puts("ホストIP : \n#{hosts_ip.to_s}\n")
+# puts("ホストサブネット : \n#{hosts_subnet.to_s}\n")
+
+fp_detail.write("[End]\n")
+puts("ファイル書き込み終了\n")
+
+# ファイルを閉じる（※必須）
+fp_topology.close
+fp_detail.close
+fp_slice.close
+fp_slice2.close
+
+
---
+++ ruby_topology/topology-controller.rb
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+$LOAD_PATH.unshift File.expand_path(File.join File.dirname(__FILE__), 'lib')
+
+require 'rubygems'
+require 'bundler/setup'
+
+require 'command-line'
+require 'topology'
+require 'trema'
+require 'trema-extensions/port'
+
+#
+# This controller collects network topology information using LLDP.
+#
+class TopologyController < Controller
+  periodic_timer_event :flood_lldp_frames, 1
+
+  def start
+    @command_line = CommandLine.new
+    @command_line.parse(ARGV.dup)
+    @topology = Topology.new(@command_line.view)
+  end
+
+  def switch_ready(dpid)
+    send_message dpid, FeaturesRequest.new
+  end
+
+  def features_reply(dpid, features_reply)
+    features_reply.physical_ports.select(&:up?).each do | each |
+      @topology.add_port dpid, each
+    end
+  end
+
+  def switch_disconnected(dpid)
+    @topology.delete_switch dpid
+  end
+
+  def port_status(dpid, port_status)
+    updated_port = port_status.port
+    return if updated_port.local?
+    @topology.update_port dpid, updated_port
+  end
+
+  def packet_in(dpid, packet_in)
+    if packet_in.lldp?
+      @topology.add_link_by dpid, packet_in
+    elsif packet_in.ipv4? || packet_in.icmpv4? ||packet_in.arp?
+      @topology.add_host_by dpid, packet_in
+      handle_ipv4 dpid, packet_in
+    end
+  end
+
+  private
+
+  def flood_lldp_frames
+    @topology.each_switch do | dpid, ports |
+      send_lldp dpid, ports
+    end
+  end
+
+  def send_lldp(dpid, ports)
+    ports.each do | each |
+      port_number = each.number
+      send_packet_out(
+        dpid,
+        actions: SendOutPort.new(port_number),
+        data: lldp_binary_string(dpid, port_number)
+      )
+    end
+  end
+
+  def lldp_binary_string(dpid, port_number)
+    destination_mac = @command_line.destination_mac
+    if destination_mac
+      Pio::Lldp.new(dpid: dpid,
+                    port_number: port_number,
+                    destination_mac: destination_mac.value).to_binary
+    else
+      Pio::Lldp.new(dpid: dpid, port_number: port_number).to_binary
+    end
+  end
+  
+# IPV4パケットの処理
+  def handle_ipv4(dpid, message)
+    if message.arp?
+      source_ip = message.arp_spa
+      dest_ip = message.arp_tpa
+      # puts "ARP from " + source_ip.to_s + " to " + dest_ip.to_s
+    else
+      source_ip = message.ipv4_saddr
+      dest_ip = message.ipv4_daddr
+      puts "IPv4 from " + source_ip.to_s + " to " + dest_ip.to_s
+    end
+    dest_host = @topology.get_host(dest_ip)
+    source_host = @topology.get_host(source_ip)
+    if dest_host.nil? || source_host.nil?
+      return
+    end
+    # 送受信ノードがホストの場合
+    if ! @topology.isInSameSlice?(source_host, dest_host, -1)
+      # 別スライスなら無視
+      puts "These hosts are not in same slice."
+      return
+    end
+    dest_dpid = dest_host.dpid1  # 宛先スイッチのdpid
+    source_dpid = source_host.dpid1  # 送信元スイッチのdpid
+    # flow_mod(dest_dpid, message, SendOutPort.new(dest_host.port1))
+    # 次の送信先ポートをダイクストラで選ぶ
+    target_dpid = source_dpid # 探索の基点
+    neighbor_dpid = nil       # 隣接ノードのdpid
+    dijkstra_stack = []       # 次にtarget_dpidにすべきスイッチ
+    dijkstra_hops = []        # 各スイッチへの暫定ホップ数
+    dijkstra_concluded = []   # 確定かどうか
+    dijkstra_walk_dpid = []   # 順路１つ前のスイッチのdpid
+    dijkstra_walk_port = []   # 順路１つ前のスイッチのポート
+    dijkstra_hops[source_dpid] = 0
+    dijkstra_concluded[source_dpid] = 1
+    #puts "dijkstra : "+source_dpid
+    puts "@" + dpid.to_s +  ", dijkstra : ["+source_host.to_s + "] -> [" + dest_host.to_s + "]"
+    while true
+      if dest_dpid==source_dpid
+        break
+      end
+      @topology.each_link do | each |
+        if each.dpid1 != target_dpid
+          next
+        end
+        puts "search link : " + each.to_s
+        neighbor_dpid = each.dpid2
+        if dijkstra_concluded[neighbor_dpid] == nil && (dijkstra_hops[neighbor_dpid] == nil || dijkstra_hops[neighbor_dpid] > dijkstra_hops[target_dpid] + 1)
+          dijkstra_hops[neighbor_dpid] = dijkstra_hops[target_dpid] + 1
+          dijkstra_walk_dpid[neighbor_dpid] = target_dpid
+          dijkstra_walk_port[neighbor_dpid] = each.port1
+          dijkstra_stack.push(neighbor_dpid)
+          puts "switch " + target_dpid.to_s + "(" +each.port1.to_s + ") -> switch " + neighbor_dpid.to_s + "  hop=" + dijkstra_hops[neighbor_dpid].to_s + "   ."
+        end
+      end
+      while true
+        next_dpid = dijkstra_stack.shift
+        if next_dpid == nil
+          break
+        elsif dijkstra_concluded[next_dpid] != nil
+          next
+        end
+        break
+      end
+      target_dpid = next_dpid
+      if next_dpid != nil
+        dijkstra_concluded[next_dpid] = 1
+      end
+      if target_dpid == nil || target_dpid == dest_dpid
+        break
+      end
+    end
+    if dijkstra_hops[dest_dpid] == nil
+      # 宛先への経路が見つからない場合
+      puts "tried dijkstra, but there is no route"
+    else
+      # 経路を元にフローテーブルを一挙更新
+        target_dpid = dest_dpid
+      while true
+        if target_dpid == source_dpid
+          break
+        end
+        puts "switch[" + dijkstra_walk_dpid[target_dpid].to_s + "] (port " + dijkstra_walk_port[target_dpid].to_s + ") -> switch[" + target_dpid.to_s + "]"
+        send_flow_mod_add(
+          dijkstra_walk_dpid[target_dpid],
+          :match => ExactMatch.from(message),
+          # :match => Match.new( :nw_dst => dest_ip.to_s, :nw_src => source_ip.to_s ),
+          # :actions => SendOutPort.new(dijkstra_walk_port[target_dpid].to_i)
+          :actions =># [
+            # Trema::SetIpDstAddr.new(dest_ip.to_s),
+            # Trema::SetEthDstAddr.new(@topology.get_mac(target_dpid)),
+            Trema::SendOutPort.new(dijkstra_walk_port[target_dpid].to_i)
+            #]
+          )
+        target_dpid = dijkstra_walk_dpid[target_dpid]
+      end
+      send_flow_mod_add(
+        dest_dpid,
+        :match => ExactMatch.from(message),
+        # :match => Match.new( :nw_dst => dest_ip.to_s, :nw_src => source_ip.to_s ),
+        # :actions => SendOutPort.new(dest_host.port1.to_i)
+        :actions => [
+          # Trema::SetIpDstAddr.new(dest_ip.to_s),
+          # Trema::SetEthDstAddr.new(dest_host.mac1),
+          Trema::SendOutPort.new(dest_host.port1.to_i)
+          ]
+        )
+	  	send_packet_out(
+	  		dest_dpid, 
+	  		:packet_in => message, 
+	  		:actions => Trema::SendOutPort.new(dest_host.port1.to_i) )
+    end
+  end
+end
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
